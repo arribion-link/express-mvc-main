9df2a25f964d908552704f229fcbcf03591a93cd
arribion <arribion@gmail.com>
2025-09-04 08:05:43 +0300

    user registration controller

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,6 +1,45 @@
+import userModel from "../models/authSchema";
+import bcrypt from 'bcrypt';
 
-export const register = (req, res) => {
-    res.send('Register Route')
+export const register = async (req, res) => {
+    const { username, email, password } = req.body;
+    try {
+        if (!username || !email || !password) {
+            return res.status(400).json({
+                message: "All fields are required!"
+            });
+        }
+
+        const userExist = await userModel.findOne({ email });
+
+        if (userExist) {
+            return res.status(400).json({
+                message: "User already exist"
+            })
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password, salt);
+
+        const userData = {
+            username,
+            email,
+            password:hashPassword
+        }
+
+        const newUser = new userModel(userData);
+        await newUser.save();
+
+        res.status(200).json({
+            success: true,
+            message:"user saves successfully"
+        })
+        
+    } catch (err) {
+         res.status(200).json({
+           success: false,
+           message: "Error Registering the user",
+         });
+    }
 }
 
 export const login = (req, res) => {
